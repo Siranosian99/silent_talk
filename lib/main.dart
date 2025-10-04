19b4b554835eca31b73d5a5a9723945dc1f5c938
MacBook <macbook@MacBooks-MacBook-Air.local>
2025-10-04 04:16:50 +0400

    notification 75?

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,17 @@ void main() async {
   await Hive.openBox('themes');
   await Hive.openBox('auth');
   await Hive.openBox('lg');
-  await NotificationHandler.initialize();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
       apiKey: "AIzaSyAwMCb9JOyl-8RCA6iPxnGlVw89AajFilc",
-      appId: "1:156704193316:android:12088ea8a9934d143211b1",
+      appId: "1:156704193316:ios:8f808066187c855d3211b1",
       //1:156704193316:ios:8f808066187c855d3211b1  //1:156704193316:android:12088ea8a9934d143211b1
       messagingSenderId: "156704193316",
       projectId: "silenttalk-53850",
     ),
   );
+   NotificationHandler.initialize();
+
   runApp(
     MultiProvider(
       providers: [
@@ -85,7 +86,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 
 // Widget build(BuildContext context) {
 //   return MaterialApp(
